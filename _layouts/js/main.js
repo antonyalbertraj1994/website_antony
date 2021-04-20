d4f0fd91ad2e9b17be8617457d4c6d589a03134f
antonyalbertraj <antonyalbertraj@gmail.com>
2021-04-19 23:49:18 -0400

    with resume

---
+++ _layouts/js/main.js
@@ -0,0 +1,18 @@
+function test(){
+  //alert('clicked');
+//  fetch('project_1.txt').then(response=>response.text()).then(text=> console.log(text));
+
+// console.log("hyp_press");
+//
+// const input=document.getElementById('file-selector');
+// console.log(input.value)
+// input.addEventListener('change',(event)=>{
+//   console.log("success");
+//   console.log(event.target.files);
+// });
+
+{}
+// var elem=document.getElementById('imagetext')
+// elem.style.color=blue;
+}
+// document.body.innerHTML="hello antony12";
